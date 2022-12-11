f7f2295eab52109ad810134398ee8ecc028bc17c
Bieber, Sarai Dana <saraidana.bieber@haw-hamburg.de>
2022-12-11 22:03:56 +0100

    Zusammenführen Sensoren und OSC

--- ESP/src/main.cpp
+++ ESP/src/main.cpp
@@ -10,15 +10,35 @@ const char ip[] = "10.10.0.66";
 
 #define UDP_PORT 3333
 WiFiUDP UDP;
-int data1 = 0;
-int data2 = 1;
-int data3 = 2;
+#define dryer_sensor 33
+#define male_sensor 17
+#define female_sensor 18
+#define sink_sensor 26
+#define trigger 34
+#define echo 35
+const int foto_max = 1023;
+int h_dryer = foto_max;
+int male = foto_max;
+int female = foto_max;
+int sink = 0;
+float duration = 0.0;
+float distance = 0.0;
+bool receive = 0;
+const float speedOfSound = 0.03432;
+
+
 
 
 
 void setup() {
   
   Serial.begin(115200);
+  pinMode(dryer_sensor, INPUT);
+  pinMode(male_sensor, INPUT);
+  pinMode(female_sensor, INPUT);
+  pinMode(sink_sensor, INPUT);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   Serial.println("set up WiFi");
   
@@ -38,13 +58,30 @@ void setup() {
 }
 
 void loop() {
-  OSCMessage msg("/data1/data2/data3");
-  msg.add(data1).add(data2).add(data3);
+  h_dryer = analogRead(dryer_sensor);
+  Serial.print("Föhn: ");
+  Serial.println(h_dryer); 
+
+  sink = analogRead(sink_sensor);
+  Serial.print("Wasserhahn: ");
+  Serial.println(h_dryer); 
+
+  male = digitalRead(male_sensor);
+  Serial.print("Badehose: ");
+  Serial.println(male);
+
+  female = digitalRead(female_sensor);
+  Serial.print("Bikini: ");
+  Serial.println(female); 
+
+
+  OSCMessage msg("/distance/h_dryer/sink/male/female");
+  msg.add(distance).add(h_dryer).add(sink).add(male).add(female);
   
   UDP.beginPacket(ip, UDP_PORT);
   msg.send(UDP);
   UDP.endPacket();
   
-  sleep(5);
+sleep(5);
   
 }
