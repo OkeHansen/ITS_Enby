b0684ab567ce7f7145c4786137960656267dac66
Bieber, Sarai Dana <saraidana.bieber@haw-hamburg.de>
2022-12-11 22:01:14 +0100

    OSC Setup

--- ESP/src/main.cpp
+++ ESP/src/main.cpp
@@ -1,77 +1,50 @@
-#include <Arduino.h>
-/* #include <WiFi.h>
-#include <WiFiUdp.h> */
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <OSCMessage.h>
 
+#define WIFI_SSID " "
+#define WIFI_PASS " "
 
-//sensors
-#define lightsensor 33
-#define button 17
-#define button_2 18
-#define trigger 34
-#define echo 35
-int button_state = 0;
-int button_2_state = 0;
-int sensed_light = 0;
-float duration = 0.00;
-float distance = 0.0;
-bool receive = 0;
-bool object = 0;
-bool light = 0;
-const float speedOfSound = 0.03432;
+//const char ip[] = "10.163.100.67";
+const char ip[] = "10.10.0.66";
 
+#define UDP_PORT 3333
+WiFiUDP UDP;
+int data1 = 0;
+int data2 = 1;
+int data3 = 2;
 
 
 
 void setup() {
+  
   Serial.begin(115200);
-  pinMode(lightsensor, INPUT);
-  pinMode(button, INPUT);
-  pinMode(button_2, INPUT);
-  pinMode(trigger, OUTPUT);
-  pinMode(echo, INPUT);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.println("set up WiFi");
+  
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
   }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  WiFi.mode(WIFI_STA);
+  /* UDP.begin(UDP_PORT); */
+  
+
+}
 
 void loop() {
-  sensed_light = analogRead(lightsensor);
-  Serial.println("Lichtsensor: ");
-  Serial.println(sensed_light); 
-  if (sensed_light < 500){
-    light = 1;
-  }else{
-    light = 0;
-  }
-
-  button_state = digitalRead(button);
-  Serial.println("Button1: ");
-  Serial.println(button_state);
-
-  button_2_state = digitalRead(button_2);
-  Serial.println("Button2: ");
-  Serial.println(button_2_state); 
-
-  digitalWrite(trigger, HIGH);  
-  delay(10);          //delay schwierig, da Code unterbrochen wird. Lieber mit millis
-  digitalWrite(trigger, LOW);
-  duration = pulseIn(echo, HIGH);
-  Serial.println("Dauer: ");
-  Serial.println(duration);
-  //distance = (duration / 2) * speedOfSound;
-  distance = duration / 58; // 2*29us für 1cm in Luft //gibt aktuell nur 0.0 zurück
-  Serial.println("Abstand: ");
-  Serial.println(distance);
-
-if(Serial.available()>0){
-    receive = Serial.read();
-    Serial.println("Recieving distance...");
-  }
+  OSCMessage msg("/data1/data2/data3");
+  msg.add(data1).add(data2).add(data3);
   
-  if(distance <= 20.0){
-    Serial.println("Object approaching...");
-    object = 1;
-    }
-
-    else{
-    Serial.println("Nothing Close...");
-    object = 0;
-    }
+  UDP.beginPacket(ip, UDP_PORT);
+  msg.send(UDP);
+  UDP.endPacket();
+  
+  sleep(5);
+  
 }
